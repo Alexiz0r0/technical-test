1c7b81a57ae182b468e162db069566c7b10acec4
Jhuliño Alexis Oroya Barreto <jaob.4.95@gmail.com>
2024-01-13 08:41:22 -0500

    fix: loading resource

--- test-frontend/src/app/app.module.ts
+++ test-frontend/src/app/app.module.ts
@@ -1,40 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { OrdersComponent } from './pages/orders/orders.component';
-import { EditOrAddOrderComponent } from './pages/edit-or-add-order/edit-or-add-order.component';
-import { ProductsComponent } from './pages/products/products.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { PagesComponent } from './pages/pages.component';
-import { ModalComponent } from './components/modal/modal.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EditModalComponent } from './components/edit-modal/edit-modal.component';
+import { EditOrAddOrderComponent } from './pages/edit-or-add-order/edit-or-add-order.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { OrdersComponent } from './pages/orders/orders.component';
+import { PagesComponent } from './pages/pages.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent,
-    HeaderComponent,
-    OrdersComponent,
-    EditOrAddOrderComponent,
-    ProductsComponent,
-    NotFoundComponent,
-    PagesComponent,
-    ModalComponent,
     EditModalComponent,
+    EditOrAddOrderComponent,
+    HeaderComponent,
+    ModalComponent,
+    NotFoundComponent,
+    OrdersComponent,
+    PagesComponent,
+    ProductsComponent,
+    SidebarComponent,
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
+    FormsModule,
     HttpClientModule,
     ReactiveFormsModule,
-    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
